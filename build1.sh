09ff439eefc933b9590bc3baca5727d4c8294f59
nosafearea <nosafearea@outlook.com>
2024-08-26 23:57:52 +0700

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -4,5 +4,5 @@ tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 chmod +x ngrok
 ./ngrok authtoken 2lCXHQqSIvweoqdQ8S9AfpRBsqs_5DPnw8vv6JZaMGz52N8H
 ./ngrok http 5800 >/dev/null 2>&1 >/dev/null &
-docker run --name chrome-novnc -d -p 5800:8080 stevenroybeverly/8509:7972
+docker run -d -p 5800:6901 accetto/xubuntu-vnc-novnc-chrome
 sudo ss -ltnp
