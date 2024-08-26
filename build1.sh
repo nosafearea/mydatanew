ad575b981358fd522eaa63167dea6b48ae10c545
nosafearea <nosafearea@outlook.com>
2024-08-26 22:49:54 +0700

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -4,6 +4,14 @@ tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 chmod +x ngrok
 ./ngrok authtoken 2lCXHQqSIvweoqdQ8S9AfpRBsqs_5DPnw8vv6JZaMGz52N8H
 ./ngrok http 5800 >/dev/null 2>&1 >/dev/null &
-docker run -d --name=chrome1 -p 5800:8080 -v /docker/appdata/chrome1:/config:rw --shm-size 5g williamrubiorita/7851:5423
+git clone https://github.com/vital987/chrome-novnc
+cd chrome-novnc
+docker build -t chrome1 .
+docker run \
+    --name chrome-novnc \
+    -e PORT=8080 \
+    -p 5800:8080 \
+    -e VNC_PASS=kune \
+    -d chrome1:latest
 sleep 2
 sudo ss -ltnp
