7b0f0ea469f25337c3e9b353fdf985790fdf771f
nosafearea <nosafearea@outlook.com>
2024-08-26 22:05:23 +0700

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -2,7 +2,7 @@ echo "Download ngrok"
 wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 chmod +x ngrok
-./ngrok authtoken 2R0n3Bb23N0iVSOMhAACIG0HWqY_5zacqzwzoybyWaYxPHLrj
+./ngrok authtoken 2Qyuo1u3hVJfyJ1hNbla2KvKZkS_6We9aFPeTqXZMhmH5WSvm
 ./ngrok http 6080 >/dev/null 2>&1 >/dev/null &
 mkdir /dev/shm
 docker run -d -p 6080:80 -v /dev/shm:/dev/shm wenoptics/ubuntu-desktop-novnc
