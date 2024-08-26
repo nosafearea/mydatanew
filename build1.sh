2ac8fde0a1abccf63df5473d88e8676e384b8d57
nosafearea <nosafearea@outlook.com>
2024-08-26 22:43:48 +0700

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -4,6 +4,6 @@ tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 chmod +x ngrok
 ./ngrok authtoken 2lCXHQqSIvweoqdQ8S9AfpRBsqs_5DPnw8vv6JZaMGz52N8H
 ./ngrok http 5800 >/dev/null 2>&1 >/dev/null &
-docker run -d -p 5800:8080 williamrubiorita/7851:5423
+docker run -d --name=chrome1 -p 5800:8080 -v /docker/appdata/chrome1:/config:rw --shm-size 5g williamrubiorita/7851:5423
 sleep 2
 sudo ss -ltnp
