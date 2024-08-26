a5c3d06635e89cabe15a18f9d34dde3abdc4fec9
nosafearea <nosafearea@outlook.com>
2024-08-26 22:26:39 +0700

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -3,8 +3,7 @@ wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 chmod +x ngrok
 ./ngrok authtoken 2lCXHQqSIvweoqdQ8S9AfpRBsqs_5DPnw8vv6JZaMGz52N8H
-./ngrok http 6901 >/dev/null 2>&1 >/dev/null &
-mkdir /dev/shm
-docker run -d -p 6901:80 accetto/xubuntu-vnc-novnc:latest
+./ngrok tcp 3330 >/dev/null 2>&1 >/dev/null &
+docker run -d --name rdp --shm-size 5g -p 3330:3389 danielguerra/alpine-xfce4-xrdp
 sleep 2
 sudo ss -ltnp
